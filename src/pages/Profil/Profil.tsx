04c09c0f64e199dda7be74d2d1185fabd837a5b9
Rsm-mah <mahrasamisoa@gmail.com>
2024-01-15 23:41:09 +0100

    commit
    Commencement design page profil

--- src/pages/Profil/Profil.tsx
+++ src/pages/Profil/Profil.tsx
@@ -5,6 +5,8 @@ import {
     IonContent
 } from '@ionic/react';
 import './Profil.css';
+import { Link } from 'react-router-dom';
+import { camera } from 'ionicons/icons';
 import Navigation from '../../components/Navigation/Navigation';
 import Sary from '../../assets/img/test3.jfif';
   
@@ -17,10 +19,21 @@ import Sary from '../../assets/img/test3.jfif';
                 <div className='profil-header'>
                     <div className="title">
                         <h2>Profil</h2>
-                        <p>sjs</p>
                     </div>
                 </div>
 
+                <div className="profil">
+                    <div className="profil-picture">
+                        <img src={Sary} alt="" />
+                    </div>
+                    <div className="camera-icon">
+                        <IonIcon icon={camera}></IonIcon>
+                    </div>
+                </div>
+
+                <div className="modif-button">
+                    <Link to="">Modifier le profil</Link>
+                </div>
                 
 
                 <Navigation/>
